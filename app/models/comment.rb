91355399cd4332ee6cd32b903545aaa4ddd549f0
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-26 18:43:21 +1000

    Added uptil view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :picture
+
+  validates :comment, presence: true
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  mount_uploader :photo, PhotoUploader
+  has_many :comments
 end
